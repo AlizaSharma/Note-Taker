74b7d34626437e8aed08870486e238e3691069d1
Aliza Sharma <aliza.sharma12@gmail.com>
2023-08-14 20:23:22 -0500

    created readandAppend variable

--- routes/api.js
+++ routes/api.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const fs = require('fs'); 
+const uuid = require('../helpers/uuid')
 
 router.get('/notes', (req, res) => {
     let notes = [];
@@ -10,5 +11,44 @@ router.get('/notes', (req, res) => {
     res.json(notes)
     
 }) 
+
+router.post('/notes', (req, res) => {
+    const { title, text } = req.body;
+
+if (title && text){
+    const newNote = {
+        title, 
+        text,
+        api_id: uuid(),
+    };
+
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+      err ? console.error(err) : console.info(`\nData written to ${destination}`)
+    );
+
+const readAndAppend = (content, file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+          if (err) {
+            console.error(err);
+          } else {
+            const parsedData = JSON.parse(data);
+            parsedData.push(content);
+            writeToFile(file, parsedData);
+          }
+        });
+      };
+
+readAndAppend(newNote, './db/db.json');
+
+const response = {
+    status: 'success', 
+    body: newNote,
+};
+res.json(response);
+} else {
+    res.json('Error in posting note');
+}
+});
 
 module.exports = router; 
--- routes/html.js
+++ routes/html.js
@@ -10,4 +10,6 @@ router.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '../public/notes.html'))
 );
 
+
+
 module.exports = router; 
